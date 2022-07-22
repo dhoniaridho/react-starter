304bd3eb7aed363425072451a7ef39fbe508e5bb
dhoniaridho <dhoniaridho@gmail.com>
2022-07-22 20:15:51 +0700

    fix: conflict

--- src/layouts/main-layout.tsx
+++ src/layouts/main-layout.tsx
@@ -1,12 +1,6 @@
-import { Helmet } from 'react-helmet'
-
 const MainLayout = ({ children }: Props) => {
   return (
     <>
-      <Helmet>
-        <meta charSet="utf-8" />
-        <link rel="canonical" href="http://mysite.com/example" />
-      </Helmet>
       <div className="main-layout">{children}</div>
     </>
   )
